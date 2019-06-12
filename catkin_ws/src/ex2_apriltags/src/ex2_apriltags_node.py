d6ad377ff5dad7d7f9f8e88feb34139f9727b2a2
awrgto <minecraftpetim@gmail.com>
2019-06-12 19:28:55 +0200

    just up-to-date

---
+++ catkin_ws/src/ex2_apriltags/src/ex2_apriltags_node.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from duckietown_msgs.msg import AprilTagsWithInfos
+
+class ex2_apriltags_node(object):
+    def __init__(self):
+
+        self.sub_tag_id = rospy.Subscriber("apriltags_postprocessing_node/apriltags_out", AprilTagsWithInfos, self.get_Apriltag, queue_size=1)
+
+    def get_Apriltag(self, Tag):
+        try:
+            tag_id = Tag.infos[0].id
+            rospy.loginfo("-------TAG = %d---------" %(tag_id))
+        except:
+            print("NO TAG")
+
+if __name__ == "__main__":
+    rospy.init_node("ex2_apriltags_node", anonymous=True)
+    ex2_apriltags = ex2_apriltags_node()
+    rospy.spin()
