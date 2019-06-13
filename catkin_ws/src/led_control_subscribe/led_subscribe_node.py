5b06e90d8234915e8e89f680d3c481291e56c34e
awrgto <minecraftpetim@gmail.com>
2019-06-13 14:41:55 +0800

    pwm

--- catkin_ws/src/led_control_subscribe/led_subscribe_node.py
+++ catkin_ws/src/led_control_subscribe/led_subscribe_node.py
@@ -12,12 +12,11 @@ class Led_subscribe(object):
 		self.sub_control = rospy.Subscriber("~led_test_control", Int32, self.sbControl, queue_size = 1)
 	
 	def sbControl(self, control_msg):
-		p = GPIO.PWM(12, 50)
+		p = GPIO.PWM(12, 100)
 		p.start(0)
 		try:
 			print("[led_subscribe_node] Subscribe successfully!")
 			print("your light level of led is %d" % control_msg.data)
-			p = GPIO.PWM(12,1)
 			p.ChangeDutyCycle(control_msg.data)
 			time.sleep(10)
 			raw_input("press any to break")
