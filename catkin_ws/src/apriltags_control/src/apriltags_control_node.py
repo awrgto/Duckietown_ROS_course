b7bc6a25e3c555c2f0b4b351cad81a8e6af70c26
owen8t8533 <minecraftmaplemax@gmail.com>
2019-06-20 10:35:20 +0800

    New apriltags_control package

---
+++ catkin_ws/src/apriltags_control/src/apriltags_control_node.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import rospy
+from duckietown_msgs.msg import AprilTagsWithInfos
+from std_msgs.msg import Int16
+
+class ex2_apriltags_node(object):
+    def __init__(self):
+        self.pub_led_brightness = rospy.Publisher("~led_brightness_controller", Int16, queue_size=1)
+        self.sub_tag_id = rospy.Subscriber("apriltags_postprocessing_node/apriltags_out", AprilTagsWithInfos, self.get_Apriltag, queue_size=1)
+        
+    def get_Apriltag(self, Tag):
+        try:
+            tag_id = Tag.infos[0].id
+            rospy.loginfo("-------TAG = %d---------" %(tag_id))
+            led_right = Int16()
+            led_right.data = 3
+            self.pub_led_brightness.publish(led_right)
+            print("led brightness have update !")
+            
+        except:
+            print("NO TAG")
+
+if __name__ == "__main__":
+    rospy.init_node("ex2_apriltags_node", anonymous=True)
+    ex2_apriltags = ex2_apriltags_node()
+    rospy.spin()
