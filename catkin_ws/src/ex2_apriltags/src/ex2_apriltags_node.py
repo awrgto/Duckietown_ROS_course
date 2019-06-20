2834ca0e2ec760086006f11cdcafe0420c26cff3
awrgto <minecraftpetim@gmail.com>
2019-06-20 13:51:35 +0800

    add

--- catkin_ws/src/ex2_apriltags/src/ex2_apriltags_node.py
+++ catkin_ws/src/ex2_apriltags/src/ex2_apriltags_node.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 import rospy
-from duckietown_msgs.msg import AprilTagsWithInfos
+from duckietown_msgs.msg import WheelsCmdStamped, AprilTagsWithInfos
 
 class ex2_apriltags_node(object):
     def __init__(self):
 
         self.sub_tag_id = rospy.Subscriber("apriltags_postprocessing_node/apriltags_out", AprilTagsWithInfos, self.get_Apriltag, queue_size=1)
+        self.sub_tag_id = rospy.Subscriberrospy.Subscriber("~wheels_cmd", WheelsCmdStamped, self.cbWheelsCmdRec, queue_size=1)
+
+        self.cwr = 0
+
+
+    def  cbWheelsCmdRec(self, wC):
+        self.cwr += wC.header.stamp.nsecs *( 10^-9 )* 20 * (wC.vel_left / 0.582576274872)
 
     def get_Apriltag(self, Tag):
         try:
+            dist = 0
             tag_id = Tag.infos[0].id
             rospy.loginfo("-------TAG = %d---------" %(tag_id))
+            if tag_id == 302 :
+                self.cwr = 0
+            elif tag_id == 21 :
+                rospy.loginfo("distance from other tag is %d cm" % (self.cwr))
+
         except:
             print("NO TAG")
 
